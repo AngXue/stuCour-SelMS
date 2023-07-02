95a82e424e441e3ccc342310812241c5154a8773
AngXue <2434789129@qq.com>
2023-07-02 14:51:22 +0800

    login界面

--- stuCourSelMS.py
+++ stuCourSelMS.py
@@ -1,1 +1,22 @@
 # 主程序
+
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from ui.login import *
+
+
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show()
+        # self.ui.loginbutton.clicked.connect()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = LoginWindow()
+    sys.exit(app.exec_())
