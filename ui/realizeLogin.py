ec337916c783ac6cb8f5289f05063a342e871161
AngXue <2434789129@qq.com>
2023-07-05 00:08:38 +0800

    小修改

--- ui/realizeLogin.py
+++ ui/realizeLogin.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import QMainWindow
 
+import logic.login as llogin
+import logic.student as lstudent
 import originalUIFile.login as ulogin
 import ui.realizeAdminHome
 import ui.realizeStuHome
 import ui.realizeTeacherHome
-import logic.student as lstudent
-import logic.teacher as lteacher
 
 
 class LoginWindow(QMainWindow):
@@ -44,18 +44,18 @@ class LoginWindow(QMainWindow):
         # 从输入框获取用户名和密码
         userName = self.ui.loginNameInput.text()
         userPasswd = self.ui.loginPasswdInput.text()
-        # userInform = llogin.login(userName, userPasswd) TODO: 调用登录函数, 返回登录信息
-        # user = gen(ID, identity) TODO: 调用生成对象函数, 返回对象
+        userInform = llogin.login(int(userName), userPasswd)  # TODO: 账号有问题
+        # user = gen(ID, identity) TODO: 缺生成对象的函数
         # id, name, college, major, grade, identity
-        # user = lstudent.Student(1, '张三', '计算机学院', '计算机科学与技术', 2, 'student')
+        user = lstudent.Student(1, '张三', '计算机学院', '计算机科学与技术', 2, 'student')
         # 教师属性：id, name, education, degree, collegeID, college, identify, major
-        user = lteacher.Teacher(2, '李四', '本科', '学士', 101, '计算机学院', 'teacher', '计算机科学与技术')
+        # user = lteacher.Teacher(2, '李四', '本科', '学士', 101, '计算机学院', 'teacher', '计算机科学与技术')
         # [True/False, id/0,'admin'/'student'/'teacher'/'wrong']
         # userInform = [True, 1, 'admin']
         # userInform = [True, 2, 'student']
-        userInform = [True, 1, 'teacher']
+        # userInform = [True, 1, 'teacher']
         # userInform = [False, 0, 'wrong']
-        if userInform[0]:  # TODO: userInform[0]
+        if userInform[0]:
             self.hide()
             if userInform[2] == 'student':
                 self.stuHome = ui.realizeStuHome.StuHome(user)
