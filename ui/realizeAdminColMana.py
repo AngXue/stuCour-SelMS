3c636f9d81605ef5352f758cdf2628185cbe94f4
AngXue <2434789129@qq.com>
2023-07-04 22:36:34 +0800

    完成管理员管理学院信息管理界面

--- ui/realizeAdminColMana.py
+++ ui/realizeAdminColMana.py
@@ -1,7 +1,7 @@
+from PyQt5 import QtGui
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QTreeWidgetItem
-from PyQt5 import QtGui
 
 import originalUIFile.adminCollegeManage as uAdminColMana
 
@@ -63,8 +63,8 @@ class AdminColManaWindow(QMainWindow):
         self.ui.showCollegeResult.setHeaderLabel('学院信息')
         # 设置初始状态全部展开
         self.ui.showCollegeResult.expandAll()
-        # 右键菜单， 鼠标右键点击学院节点，弹出菜单，可以添加专业、删除学院
-        # 鼠标右键点击专业节点，弹出菜单，可以删除专业、添加培养方案、删除培养方案、查看培养方案
+        # 右键菜单， 鼠标右键点击学院节点，弹出菜单，可以删除学院
+        # 鼠标右键点击专业节点，弹出菜单，可以删除专业、查看培养方案
         self.ui.showCollegeResult.setContextMenuPolicy(3)
         self.ui.showCollegeResult.customContextMenuRequested.connect(self.showCollegeResultMenu)
 
@@ -77,9 +77,6 @@ class AdminColManaWindow(QMainWindow):
         # 判断选中的是学院还是专业
         # 选中的是学院
         if self.ui.showCollegeResult.currentItem().parent() is None:
-            # 添加专业
-            addMajorAction = menu.addAction('添加专业')
-            addMajorAction.triggered.connect(self.addMajor)
             # 删除学院
             delCollegeAction = menu.addAction('删除学院')
             delCollegeAction.triggered.connect(self.delCollege)
@@ -88,12 +85,6 @@ class AdminColManaWindow(QMainWindow):
             # 删除专业
             delMajorAction = menu.addAction('删除专业')
             delMajorAction.triggered.connect(self.delMajor)
-            # 添加培养方案
-            addTrainProgramAction = menu.addAction('添加培养方案')
-            addTrainProgramAction.triggered.connect(self.addTrainProgram)
-            # 删除培养方案
-            delTrainProgramAction = menu.addAction('删除培养方案')
-            delTrainProgramAction.triggered.connect(self.delTrainProgram)
             # 查看培养方案
             showTrainProgramAction = menu.addAction('查看培养方案')
             showTrainProgramAction.triggered.connect(self.showTrainProgram)
@@ -105,15 +96,6 @@ class AdminColManaWindow(QMainWindow):
         :return:
         """
         pass
-
-    def addMajor(self):
-        """
-        添加专业
-        :return:
-        """
-        # 获取学院ID
-        collegeID = self.ui.showCollegeResult.currentItem().text(0).split('[')[1].split(']')[0]
-        # self.admin.addMajor(collegeID) # TODO: 连接数据库
 
     def delMajor(self):
         """
@@ -133,23 +115,3 @@ class AdminColManaWindow(QMainWindow):
         # 获取学院ID
         collegeID = self.ui.showCollegeResult.currentItem().text(0).split('[')[1].split(']')[0]
         # self.admin.delCollege(collegeID) # TODO: 连接数据库
-
-    def addTrainProgram(self):
-        """
-        添加培养方案
-        :return:
-        """
-        # 获取专业所属学院ID和专业名称
-        collegeID = self.ui.showCollegeResult.currentItem().parent().text(0).split('[')[1].split(']')[0]
-        majorName = self.ui.showCollegeResult.currentItem().text(0)
-        # self.admin.addTrainProgram(collegeID, majorName) # TODO: 连接数据库
-
-    def delTrainProgram(self):
-        """
-        删除培养方案
-        :return:
-        """
-        # 获取专业所属学院ID和专业名称
-        collegeID = self.ui.showCollegeResult.currentItem().parent().text(0).split('[')[1].split(']')[0]
-        majorName = self.ui.showCollegeResult.currentItem().text(0)
-        # self.admin.delTrainProgram(collegeID, majorName) # TODO: 连接数据库
